d3a4336a3aee0731fd6730f668e78a70eba25de5
David H <iq.davidh@gmail.com>
2019-05-14 06:56:35 -0500

    en ms funcionando

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,110 @@
+class Lapso {
+
+    constructor() {
+        this.ms = 0;
+        this.s = 0;
+        this.min = 0;
+    }
+
+    getTextoMS() {
+        return (
+            (this.ms < 100 ? '0' : '') +
+            (this.ms < 10 ? '0' : '')
+            + this.ms.toString()
+        );
+    }
+
+    getTextoS() {
+        return (this.s < 10 ? '0' : '') + this.s.toString();
+    }
+
+    getTextoMin() {
+        return (this.min < 10 ? '0' : '') + this.min.toString();
+    }
+
+    addSegundo() {
+
+        this.s++;
+
+        if (this.s === 60) {
+            this.min++;
+            this.s = 0;
+        }
+
+    }
+
+    add10MiliSegundo() {
+
+        this.ms+=10;
+
+        if (this.ms === 1000) {
+            this.addSegundo();
+            this.ms=0;
+        }
+
+    }
+}
+
+
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+
+
+    constructor(callBackOnAvance) {
+
+
+        this.callBackOnAvance = callBackOnAvance;
+
+        this.inicializarLapso();
+
+        this.listaLapsos = [];
+
+        this.intervaloMS = 10; //se cuenta por segundos
+        this.intervalId = 0;
+    }
+
+    inicializarLapso() {
+        this.lapso = new Lapso();
+    }
+
+
+    start_or_stop() {
+
+        if (this.intervalId === 0) {
+            //Esta en STOP asi que hacemos un start
+            this.inicializarLapso();
+
+            let fnOnAvanzaCronometro = () => {
+                this.lapso.add10MiliSegundo()
+                this.callBackOnAvance(this.lapso);
+            };
+
+            this.intervalId = setInterval(fnOnAvanzaCronometro, this.intervaloMS);
+
+        } else {
+
+            //estamos en RUN asi que hacemos stop
+            clearTimeout(this.intervalId);
+            this.intervalId = 0;
+
+        }
+
+
+    }
+
+    getModo() {
+        return this.intervalId === 0 ? 'STOP' : 'RUNNING';
+    }
+
+    reset() {
+
+        this.inicializarLapso();
+
+        this.intervalId = 0;
+        clearTimeout(this.intervalId);
+
+        this.listaLapsos = [];
+
+    }
+
+
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,53 @@
-var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
 
 
-function printTime() {
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
 
-}
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
 
-function printMinutes() {
+var milCentena = document.getElementById('milCentena');
 
-}
 
-function printSeconds() {
 
-}
+var callbackOnAvanza1Segundo = (lapso) => {
 
-function printMilliseconds() {
+    console.log(lapso);
 
-}
+    let ms=lapso.getTextoMS();
+    let s=lapso.getTextoS();
+    let m=lapso.getTextoMin();
 
-function printSplit() {
 
-}
+    /* resuilta que la fuente no es mono espace y debemos dvidid
+    * cada digito en un span
+    * */
 
-function clearSplits() {
+    milCentena.innerText=ms.substr(0,1);
 
-}
 
-function setStopBtn() {
+    secDec.innerText = s.substr(0,1);
+    secUni.innerText = s.substr(1);
+    minDec.innerText = m.substr(0,1);
+    minUni.innerText = m.substr(1);
 
-}
 
-function setSplitBtn() {
 
-}
+};
 
-function setStartBtn() {
 
-}
+var cronometro = new Chronometer(callbackOnAvanza1Segundo);
 
-function setResetBtn() {
 
-}
 
-// Start/Stop Button
-btnLeft.addEventListener('click', function () {
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
 
-});
+btnLeft.onclick=(event)=>{
+  cronometro.start_or_stop();
+};
 
-// Reset/Split Button
-btnRight.addEventListener('click', function () {
 
-});
+btnRight.onclick=(event)=>{
+    cronometro.reset();
+};
